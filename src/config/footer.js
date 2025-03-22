7fdc4b8118afe92bf85558ce7388dcbcc40f166a
Josef Bouše <github@josefbouse.cz>
2025-03-22 21:46:56 +0100

    first commit

---
+++ src/config/footer.js
@@ -0,0 +1,43 @@
+export const footerSocials = [
+	{
+		name: 'Mastodon',
+		url: 'https://witter.cz/@Hnutikruh',
+		icon: "mdi:mastodon",
+	},
+	{
+		name: 'BlueSky',
+		url: 'https://bsky.app/profile/HnutiKruh',
+		icon: "simple-icons:bluesky",
+	},
+	{
+		name: 'Threads',
+		url: 'https://threads.net/@HnutiKruh',
+		icon: "simple-icons:threads",
+	},
+	{
+		name: 'Instagram',
+		url: 'https://instagram.com/HnutiKruh',
+		icon: "mdi:instagram",
+	},
+];
+
+export const footerLists = [
+	{
+		title: 'Kontakt',
+		items: [
+			{
+				title: 'marta@martamartinova.cz',
+				slug: 'mailto:marta@martamartinova.cz',
+			},
+			{
+				title: 'info@janakolarikova.cz',
+				slug: 'mailto:info@janakolarikova.cz',
+			},
+			{
+				title: 'klara@kadar.cz',
+				slug: 'mailto:klara@kadar.cz',
+			},
+		],
+	},
+];
+
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
+	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	url: `https://hnutikruh.cz`, // No trailing slash!
+	name: `Hnutí kruh`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: false,
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+};

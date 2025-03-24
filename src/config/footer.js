d34870aa27a316084f66dfaebe41f80cf25f1df4
Josef Bouše <github@josefbouse.cz>
2025-03-24 22:10:15 +0100

    Uprava Footeru, podpora kontaktu v komponentě, uprava stylu pro pouze 2 sloupce.

--- src/config/footer.js
+++ src/config/footer.js
@@ -1,6 +1,6 @@
 export const footerSocials = [
 	{
-		
+
 		name: 'Mastodon',
 		url: 'https://witter.cz/@Hnutikruh',
 		icon: "mastodonIcon",
@@ -24,56 +24,24 @@ export const footerSocials = [
 
 
 export const footerLists = [
+
+];
+
+export const contactList = [
 	{
-		title: 'Kontakt',
-		items: [
-			{
-				title: 'Marta Martinová',
-				/* slug: 'mailto:marta@martamartinova.cz', */
-			},
-			{
-				title: 'Jana Koláříková',
-				/* slug: 'mailto:info@janakolarikova.cz', */
-			},
-			{
-				title: 'Klára Kadár',
-				/* slug: 'mailto:klara@kadar.cz', */
-			},
-		],
+		name: 'Marta Martinová',
+		email: 'marta@martamartinova.cz',
+		phone: '+420 724 020 585',
 	},
 	{
-		title: 'Email',
-		items: [
-			{
-				title: 'marta@martamartinova.cz',
-				slug: 'mailto:marta@martamartinova.cz',
-			},
-			{
-				title: 'info@janakolarikova.cz',
-				slug: 'mailto:info@janakolarikova.cz',
-			},
-			{
-				title: 'klara@kadar.cz',
-				slug: 'mailto:klara@kadar.cz',
-			},
-		],
+		name: 'Jana Koláříková',
+		email: 'info@janakolarikova.cz',
+		phone: '+420 722 419 534',
 	},
 	{
-		title: 'Telefon',
-		items: [
-			{
-				title: '+420 724 020 585',
-				slug: 'tel:+420724020585',
-			},
-			{
-				title: '+420 722 419 534',
-				slug: 'tel:+420722419534',
-			},
-			{
-				title: '+420 736 456 457',
-				slug: 'tel:+420736456457',
-			},
-		],
+		name: 'Klára Kadár',
+		email: 'klara@kadar.cz',
+		phone: '+420 736 456 457',
 	},
 ];
 
